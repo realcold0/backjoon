04ed1edf2e23e22934a1ba21cffec6c18e1d67d2
realcold0 <65608503+realcold0@users.noreply.github.com>
2023-01-08 22:49:03 +0900

    [Bronze V] Title: A+B, Time: 216 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a, b, sum;
+        Scanner scanner = new Scanner(System.in);
+
+        a  = scanner.nextInt();
+        b = scanner.nextInt();
+
+        sum = a + b;
+
+        System.out.println(sum);
+
+    }
+}
