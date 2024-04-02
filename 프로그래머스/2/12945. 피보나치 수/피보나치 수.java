10a83d00f41ad4e142f1ae9b4a42b15ef2e645b0
realcold0 <65608503+realcold0@users.noreply.github.com>
2024-04-02 23:48:33 +0900

    [level 2] Title: 피보나치 수, Time: 3.38 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -6,7 +6,7 @@ class Solution {
             if(i<2){
                 fibo[i]=i;
             }else{
-                fibo[i]= (fibo[i-1]+fibo[i-2])%1234567;
+                fibo[i]= (fibo[i-1] %1234567 +fibo[i-2] %1234567 )%1234567;
             }
         }
         return fibo[n];  
