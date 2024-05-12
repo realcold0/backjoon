2856865ca748c4621321c1fb57655b25566bd968
realcold0 <65608503+realcold0@users.noreply.github.com>
2024-05-12 22:34:46 +0900

    [Gold V] Title: 탑, Time: 856 ms, Memory: 84816 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static class Building{
+        int top;
+        int order;
+        public Building(int top, int order){
+            this.top = top;
+            this.order = order;
+        }
+    }
+    public static void main(String[]args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        Stack<Building> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1 ; i <= n ;i++){
+            int buildingTop = Integer.parseInt(st.nextToken());
+
+            if(stack.isEmpty()){
+               sb.append("0 ");
+               stack.push(new Building(buildingTop, i));
+            }else{
+                while(true){
+                    if(stack.isEmpty()){
+                        sb.append("0 ");
+                        stack.push(new Building(buildingTop, i));
+                        break;
+                    }
+                    if (stack.peek().top > buildingTop){ //스택이 더크다
+                        sb.append(stack.peek().order).append(" ");
+                        stack.push(new Building(buildingTop, i));
+                        break;
+                    }
+                    else{ //낵가 더크다
+                        stack.pop();
+                    }
+                }
+
+            }
+
+
+
+        }
+
+        System.out.println(sb);
+    }
+
+
+
+
+
+}
