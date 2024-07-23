5a2c4bf75106f9dc5968a4e1fb68e76138d2d3ea
realcold0 <65608503+realcold0@users.noreply.github.com>
2024-07-23 11:15:23 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,8 @@
+-- DEVELOPERS 테이블에서 Python이나 C# 스킬을 가진 개발자의 정보를 조회하려 합니다. 
+-- 조건에 맞는 개발자의 ID, 이메일, 이름, 성을 조회하는 SQL 문을 작성해 주세요.
+SELECT D.ID, D.EMAIL, D.FIRST_NAME, D.LAST_NAME
+FROM DEVELOPERS D
+WHERE D.SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME = 'Python')
+OR
+D.SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME = 'C#')
+ORDER BY D.ID
